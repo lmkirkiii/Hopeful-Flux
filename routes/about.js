a09fa22d2806a1a5257aa42ddf78037b5c203d1a
Lawrence Kirk <lmkirkiii@gmail.com>
2018-07-28 18:02:55 -0400

    switching to a new brancho

--- routes/about.js
+++ routes/about.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const aboutController = require('../controllers/about.js');
-
-router.get('/', aboutController.show);
+    
+router.get('/about', aboutController.show);
 
 module.exports = router;
