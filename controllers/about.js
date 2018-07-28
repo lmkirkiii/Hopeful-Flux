e574dbb71fea7369b646208a38f37313b1a9f107
Lawrence Kirk <lmkirkiii@gmail.com>
2018-07-28 17:55:53 -0400

    trying to get about to work

---
+++ controllers/about.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+
+  show: (req, res) => {
+        event.preventDefault()
+        res.render("/about", { info });
+        console.log("working")
+  }
+
+};
---
+++ routes/about.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const aboutController = require('../controllers/about.js');
+
+router.get('/', aboutController.show);
+
+module.exports = router;
